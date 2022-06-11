9d95815b954717c52d74e5a57584317e841134f8
caglarorhan <caglaror@gmail.com>
2022-06-11 17:35:42 -0500

    Singly Linked Lists - reverse method

---
+++ data_structures/singly_linked_lists/reversing_sll.js
@@ -0,0 +1,55 @@
+class Node{
+    constructor(val){
+        this.data=val;
+        this.next=null;
+    }
+}
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    reverse(){
+        if(!this.head){
+            return null;
+        }else{
+            let current = this.head;
+            while(current.next){
+                let target= current.next;
+                target.next ='';
+            }
+        }
+    }
+    printList(){
+        let current = this.head;
+        while(current){
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+let newLinkedList = new SinglyLinkedList();
+newLinkedList.push(1);
+newLinkedList.push(2);
+newLinkedList.push(3);
+newLinkedList.push(4);
+newLinkedList.printList();
+newLinkedList.reverse();
+newLinkedList.printList();
+
+
+
